06d4d143bc26a5ffb79c31cfbb31a395014e5617
Rodrigo Adriano Prestes <rodrigoaprestes@gmail.com>
2025-05-29 15:04:25 -0300

    commit

--- src/app/data.tsx
+++
@@ -1,9 +0,0 @@
-import { SlArrowDown } from "react-icons/sl";
-
-export const icons = [
-  { name: "About" },
-  { name: "Services" },
-  { name: "Skills" },
-  { name: "Projects" },
-  { name: "Contact" },
-];
